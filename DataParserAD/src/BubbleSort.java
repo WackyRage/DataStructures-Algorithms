e51d05ca53bff1c343407a1091816f612a717649
Martin Westerhof <a.m.westerhof@gmail.com>
2024-03-15 17:11:34 +0100

    Create BubbleSort.java

---
+++ DataParserAD/src/BubbleSort.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class BubbleSort {
+    static void bubbleSort(ArrayList<Game> games) {
+        int size = games.size();
+
+        // Loop through all game elements
+        for (int i = 0; i < size - 1; i++) {
+
+            // Element compare loop
+            for (int j = 0; j < size - i - 1; j++){
+                Game currentGame = games.get(j);
+                Game nextGame = games.get(j + 1);
+
+                //compare elements
+                if (currentGame.getPrice() > nextGame.getPrice()) {
+
+                    games.set(j, nextGame);
+                    games.set(j + 1, currentGame);
+                }
+            }
+        }
+    }
+}
